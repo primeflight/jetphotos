eddea0f9924dca4b4640ca1c4da71f677fdc25dc
rodriguesfas <franciscosouzaacer@gmail.com>
2023-08-13 10:15:24 -0300

    added test

--- tests/tests_search.py
+++ tests/tests_search.py
@@ -1,16 +1,24 @@
 import unittest
-from unittest.mock import Mock, patch
+from unittest.mock import patch
 from jetphotos.search import Search
 
 
 class TestSearch(unittest.TestCase):
     @patch("jetphotos.search.requests.get")
     def test_aircraft(self, mock_get):
-        mock_response = Mock()
-        mock_response.content = b'<html><div class="result__section result__section--photo-wrapper"><img src="//example.com/photo.jpg"></div></html>'
-        mock_get.return_value = mock_response
+        mock_response = mock_get.return_value
+        mock_response.content = b"<html>"
+        mock_response.content += b'<div class="result__section result__section--photo-wrapper">'
+        mock_response.content += b'<img src="//example.com/photo.jpg">'
+        mock_response.content += b"</div>"
+        mock_response.content += b"</html>"
 
-        expected_url = "https://example.com/photo.jpg"
         result_url = Search.aircraft(prefix="PT-RVT")
 
+        expected_url = "https://example.com/photo.jpg"
+
         self.assertEqual(result_url, expected_url)
+
+
+if __name__ == "__main__":
+    unittest.main()
